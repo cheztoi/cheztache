3f83fade70b0e0b39a69ad1dc7f6838086cfde3a
Kelvin Hammond <hammond.kelvin@gmail.com>
2015-07-20 19:23:36 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='Chez Tache',
+    version='0.0.1',
+    description='A home for your tasks. A task manager.',
+    author='Kelvin Hammond',
+    author_email='hammond.kelvin@gmail.com',
+    url='',
+    packages=['cheztache'],
+    platforms='any',
+    entry_points={
+        'console_scripts': [
+            'cheztache = cheztache.commands:cli',
+        ]
+    },
+)
