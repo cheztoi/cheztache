5388a3d3322e03374db7739a67f7c1fcd6aeaace
Kelvin Hammond <hammond.kelvin@gmail.com>
2015-07-23 23:18:34 -0400

    Added done command and fixed list command

--- chez/tache/commands.py
+++ chez/tache/commands.py
@@ -1,5 +1,7 @@
 import click
 import arrow
+import sqlalchemy
+from sqlalchemy.sql import or_
 from tabulate import tabulate
 from .factory import create_app
 from .models import Task, Project
@@ -49,7 +51,8 @@ def list(ctx, app, projects, arguments):
         defaults = ''
         query = ts.filter_by_arguments(arguments, defaults=defaults)
         query = query.filter(Task.completed == None)  # noqa
-        query = query.filter(Task.waituntil <= arrow.now())
+        query = query.filter(
+            or_(Task.waituntil <= arrow.now(), Task.waituntil == None))  # noqa
         if query.count():
             table = {
                 '#': [],
@@ -63,6 +66,39 @@ def list(ctx, app, projects, arguments):
             click.echo(tabulate(table, headers="keys"))
         else:
             click.echo("No matching tasks")
+
+
+@cli.command()
+@click.pass_obj
+@click.pass_context
+@click.argument('ids', nargs=-1)
+def done(ctx, app, ids):
+    arguments = ids
+    ids = []
+    for arg in arguments:
+        for i in arg.split(','):
+            if i:
+                try:
+                    ids.append(int(i))
+                except ValueError:
+                    click.echo('Invalid task number: {}'.format(i))
+                    ctx.exit()
+    ids = set(ids)
+    if not ids:
+        return ctx.fail("No tasks ids defined")
+
+    with app.app_context():
+        tasks = []
+        for i in ids:
+            try:
+                task = Task.query.filter(Task.number == i).one()
+                tasks.append(task)
+            except sqlalchemy.orm.exc.NoResultFound:
+                click.echo("Invalid task id: {}".format(i))
+                ctx.exit()
+
+        for task in tasks:
+            task.delete()
 
 
 @cli.group()
