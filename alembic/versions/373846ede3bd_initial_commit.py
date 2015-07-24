84a64151c50e83b692a3a2abd1a13738d3993ba3
Kelvin Hammond <hammond.kelvin@gmail.com>
2015-07-23 20:21:45 -0400

    Added alembic

---
+++ alembic/versions/373846ede3bd_initial_commit.py
@@ -0,0 +1,24 @@
+"""initial commit
+
+Revision ID: 373846ede3bd
+Revises: 
+Create Date: 2015-07-23 20:21:11.061034
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '373846ede3bd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
